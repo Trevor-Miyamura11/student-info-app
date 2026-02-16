64e9e3c47c293777a82bc0bb3cc1f2f35705b6d8
Trevor Miyamura <trevor.miyamura11@gmail.com>
2026-02-16 19:05:29 +0800

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/student-info-app/", // Use your repo name here
   plugins: [
     vue(),
     vueDevTools(),
